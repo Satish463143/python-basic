8b9e853e119a5cf86aabf8a18b35bb77e3c1ea59
Satish <mahatosatish463@gmail.com>
2025-11-26 22:32:08 +0545

    learned string slice, string method, square

--- first.py
+++ first.py
@@ -1,1 +1,42 @@
-print ("hello mf")
+# basic python program to import a module
+import pyjokes
+# print ("hello b")
+
+# joke = pyjokes.get_joke()
+# print(joke)
+
+# operators
+
+# a= 10
+# b=21
+
+# print (b/a)
+
+############### type conveersion
+# a = "satish"
+# b = type(a)
+# print(b)
+
+# c = 23
+# d = float(c)
+# t = type(d)
+# print(t)
+
+############## input function
+# e = int (input ("enter first number: "))
+# # f = int(input ("enter second number: "))
+
+############ square of a number
+# print ((e**2))
+
+############ string slicing
+name= "satish mahato"
+length = len(name)
+shortName = name[-5:-4]
+middlevalue = name[1:9:2]
+print(middlevalue)
+
+############## string methods
+name = "satish kuamr"
+print (name.endswith("mar"))
+print (name.capitalize())
